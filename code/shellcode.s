5bedaccf558b785074828a60232f248ab9c63d3e
John Heidal <jpheidal@gmail.com>
2020-11-06 02:18:44 +0000

    translated shellcode to hex

--- code/shellcode.s
+++ code/shellcode.s
@@ -1,7 +1,5 @@
-.pos 0x0
                  gpc  $0x8, r0            # r0 = location of /bin/sh
                  ld   $0x8, r1            # r1 = length of /bin/sh
                  sys  $2                  # run /bin/sh
-.pos 0xa
                  .long 0x2f62696e
                  .long 0x2f736800
